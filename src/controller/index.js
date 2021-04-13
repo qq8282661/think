55ee41387b9e365bf2ebd235d33d938f9132db28
huazai5m <644827059@qq.com>
2021-04-13 11:36:52 +0800

    ++++

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,4 +1,11 @@
+const { think } = require('thinkjs');
 const Base = require('./base.js');
+
+for (let i = 1; i < 10000; i++) {
+  think
+    .model('agent_company')
+    .add({ agent_id: i, title: i, manager_uid: i, spcode: i, parent_id: i - 1 });
+}
 
 module.exports = class extends Base {
   async indexAction() {
--- src/model/user.js
+++ src/model/user.js
@@ -1,7 +1,7 @@
 module.exports = class extends think.Model {
   get relation() {
     return {
-      cat: think.Model.HAS_MANY,
+      // cat: think.Model.HAS_MANY,
       // profile: think.Model.BELONGS_TO,
     };
   }
